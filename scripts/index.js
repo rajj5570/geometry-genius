8151c549fc1c0ae7b9595757f71a8bc8795dbcac
rajib5570 <rajib5570@gmail.com>
2024-02-13 17:06:49 +0600

    dynamic triangle added

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,50 @@
-function triangleAreaCalc(){
-    const base = areaCalc('triangle-base');
-    const height = areaCalc('triangle-height');
-    const area = (base * height)/2;
+function triangleCalc(){
+    const base = getValueById('triangle-base');
+    const height = getValueById('triangle-height');
+    const area = base * height;
+    showResult('result',area);
     return area;
-}
-
-function areaCalc(inputId){
-    const inputField =  document.getElementById(inputId);
-    const inputText = inputField.value;
-    const value = parseFloat(inputText);
-return value;
 
 }
+function showResult(element,area){
+    const result = document.getElementById(element);
+    result.innerHTML = area+' cm<sup>2</sup>';
+}
+
+function getValueById(elementID){
+    const inputField = document.getElementById(elementID);
+    const inputValue = inputField.value;
+    const value = parseFloat(inputValue);
+    return value;
+}
+
+
+
+
+
+// function triangleCalc(){
+//     const inputBase = document.getElementById('triangle-base');
+//     const inputBaseText = inputBase.value;
+//     const base = parseFloat(inputBaseText);
+//     console.log(base);
+
+//     const inputHeight = document.getElementById('triangle-height');
+//     const inputHeightText = inputHeight.value;
+//     const height = parseFloat(inputHeightText);
+//     console.log(height);
+    
+
+//     const area = (base*height)/2;
+//     console.log(area);
+//     triangleResult(area);
+//     return area;
+    
+
+    
+// }
+
+// function triangleResult(area){
+//     const triangle = document.getElementById('result');
+//     triangle.innerHTML= area+'cm<sup>2</sup>';
+
+// }
