3ed1ea179f177b4e256ac0c288cea653298f898a
rajib5570 <rajib5570@gmail.com>
2024-02-13 22:53:18 +0600

    all dynamic card added

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 
-
+// triangle function
 function triangleCalc(){
     const base = getValueById('triangle-base');
     const height = getValueById('triangle-height');
@@ -8,7 +8,7 @@ function triangleCalc(){
     return area;
 
 }
-
+// rectangle function
 function rectangleCalc(){
     const width = getValueById('rectangle-width');
     const height = getValueById('rectangle-height');
@@ -17,17 +17,70 @@ function rectangleCalc(){
     return area;
 }
 
+// parallelogram function
+function parallelogramCalc(){
+    const base = getValueById('parallelogram-base');
+    const height = getValueById('parallelogram-height');
+    const area = base * height;
+    showResult('result-parallelogram',area);
+    return area;
+}
+
+// rhombus function
+function rhombusCalc(){
+    const d1 =getValueById('rhombus-d1');
+    const d2 =getValueById('rhombus-d2');
+    const area = d1 * d2;
+    showResult('result-rhombus',area);
+    return area;
+}
+
+// pentagon function
+function pentagonCalc(){
+    const perimeter = getValueById('pentagon-perimeter');
+    const apothem = getValueById('pentagon-apothem');
+    const area = (perimeter*apothem)/2;
+    showResult('result-pentagon',area);
+    return area;
+}
+
+// ellipse function
+function ellipseCalc(){
+    const a = getValueById('a');
+    const b = getValueById('b');
+    const area = 3.14*a*b;
+    showResult('result-ellipse',area);
+    return area;
+}
+
+// common function for getting parameter value from a figure
 function getValueById(elementID){
     const inputField = document.getElementById(elementID);
     const inputValue = inputField.value;
     const value = parseFloat(inputValue);
     return value;
+  
 }
 
+// common function for showing result on html
 function showResult(element,area){
-    const result = document.getElementById(element);
-    result.innerHTML = area+' cm<sup>2</sup>';
+    if(area >0){
+        const result = document.getElementById(element);
+        result.innerHTML = area+' cm<sup>2</sup>';
+    }else{
+        alert('invalid input');
+        return;
+    }
 }
+
+// common function for hover background
+const hoverEffect = document.getElementsByClassName('card');
+hoverEffect.addEventListener('onmousemove',function(){
+    hoverEffect.style.background = "#FBBCED";
+})
+
+
+
 
 
 
